18a4591b9929d0de13b6b4213948e512e57a3467
legiz-ru <legiz.ru@gmail.com>
2025-09-18 09:34:22 +0300

    Use machine ID for HWID and normalize OS header

--- src-go/pkg/utils/httpclient_test.go
+++ src-go/pkg/utils/httpclient_test.go
@@ -7,15 +7,13 @@ import (
 func TestGenerateHWID(t *testing.T) {
 	hwid1 := generateHWID()
 	hwid2 := generateHWID()
-	
-	// HWID should be unique
-	if hwid1 == hwid2 {
-		t.Errorf("HWID should be unique, got same HWID: %s", hwid1)
+
+	if hwid1 == "" {
+		t.Fatal("HWID should not be empty")
 	}
-	
-	// HWID should be 36 characters (UUID format)
-	if len(hwid1) != 36 {
-		t.Errorf("HWID should be 36 characters, got %d", len(hwid1))
+
+	if hwid1 != hwid2 {
+		t.Errorf("HWID should be consistent across calls, got '%s' and '%s'", hwid1, hwid2)
 	}
 }
 
@@ -25,12 +23,12 @@ func TestBuildDeviceHeaders(t *testing.T) {
 		EnableHWID: false,
 	}
 	UpdateHTTPClientConfig(config)
-	
+
 	headers := buildDeviceHeaders()
 	if headers != nil {
 		t.Errorf("Expected nil headers when HWID is disabled, got %v", headers)
 	}
-	
+
 	// Test with HWID enabled
 	config = &HTTPClientConfig{
 		EnableHWID:  true,
@@ -39,26 +37,37 @@ func TestBuildDeviceHeaders(t *testing.T) {
 		DeviceModel: "TestDevice",
 	}
 	UpdateHTTPClientConfig(config)
-	
+
 	headers = buildDeviceHeaders()
 	if headers == nil {
 		t.Fatal("Expected headers when HWID is enabled, got nil")
 	}
-	
+
 	if _, exists := headers["x-hwid"]; !exists {
 		t.Error("Expected x-hwid header")
 	}
-	
+
 	if headers["x-device-os"] != "Linux" {
 		t.Errorf("Expected x-device-os to be 'Linux', got '%s'", headers["x-device-os"])
 	}
-	
+
 	if headers["x-ver-os"] != "5.4.0" {
 		t.Errorf("Expected x-ver-os to be '5.4.0', got '%s'", headers["x-ver-os"])
 	}
-	
+
 	if headers["x-device-model"] != "TestDevice" {
 		t.Errorf("Expected x-device-model to be 'TestDevice', got '%s'", headers["x-device-model"])
+	}
+
+	config = &HTTPClientConfig{
+		EnableHWID: true,
+		DeviceOS:   "Windows x64",
+	}
+	UpdateHTTPClientConfig(config)
+
+	headers = buildDeviceHeaders()
+	if headers["x-device-os"] != "Windows" {
+		t.Errorf("Expected x-device-os to be 'Windows', got '%s'", headers["x-device-os"])
 	}
 }
 
@@ -69,18 +78,18 @@ func TestUpdateHTTPClientConfig(t *testing.T) {
 		Version:    "1.0.1",
 	}
 	UpdateHTTPClientConfig(config)
-	
+
 	expected := "prizrak-box/1.0.1"
 	if globalConfig.UserAgent != expected {
 		t.Errorf("Expected user agent '%s', got '%s'", expected, globalConfig.UserAgent)
 	}
-	
+
 	// Test user agent with HWID disabled
 	config = &HTTPClientConfig{
 		EnableHWID: false,
 	}
 	UpdateHTTPClientConfig(config)
-	
+
 	expected = "clash-verge/v2.3.0"
 	if globalConfig.UserAgent != expected {
 		t.Errorf("Expected user agent '%s', got '%s'", expected, globalConfig.UserAgent)
