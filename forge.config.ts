f4e468af53f8415499c519ab118c503487ae4e6c
legiz-ru <legiz.ru@gmail.com>
2025-10-23 12:26:57 +0300

    update 23102025 add archlinux aur release add setup .exe for windows update templates update dns override template add window about connections log and copy button

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,6 @@
 import type {ForgeConfig} from '@electron-forge/shared-types';
 import {MakerWix} from '@electron-forge/maker-wix';
+import {MakerSquirrel} from '@electron-forge/maker-squirrel';
 import {MakerDMG} from '@electron-forge/maker-dmg';
 import {MakerDeb} from '@electron-forge/maker-deb';
 import {MakerRpm} from '@electron-forge/maker-rpm';
@@ -29,6 +30,12 @@ const config: ForgeConfig = {
     },
     rebuildConfig: {},
     makers: [
+        new MakerSquirrel({
+            name: 'Prizrak-Box',
+            authors: ['legiz-ru'],
+            setupIcon: 'build/appicon.ico',
+            iconUrl: 'https://raw.githubusercontent.com/legiz-ru/Prizrak-Box/prizrak_dev/build/appicon.ico',
+        }),
         new MakerWix({
             manufacturer: 'legiz-ru',
             description: 'A Simple Mihomo GUI',
